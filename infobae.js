4d28fa6e0c937536ee727e64bf31243ef32ed9ab
joserost999 <joserost999@gmail.com>
2018-04-06 19:20:50 -0300

    -

---
+++ infobae.js
@@ -0,0 +1,17 @@
+window.infobae = 
+{
+"news" : [
+		{
+			"title":"INFO - bae ",
+			"subtitle": "que tal al;sd aks dal;sdk l;as d"
+		},
+		{
+			"title":"holchaua carola2 ",
+			"subtitle": "que tal2"
+		},
+		{
+			"title":"holaas da  carola3",
+			"subtitle": "que tal3"
+		}
+	]
+}
---
+++ ln.js
@@ -0,0 +1,17 @@
+window.ln = 
+{
+"news" : [
+		{
+			"title":"infobae 1",
+			"subtitle": "que tal"
+		},
+		{
+			"title":"infobae 2 ",
+			"subtitle": "que tal2"
+		},
+		{
+			"title":"infobae 3",
+			"subtitle": "que tal3"
+		}
+	]
+}
